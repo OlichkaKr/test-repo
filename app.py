d3841740ae8b94ca8fde398480d3fe94dab8e182
Olga Kr <olichka121kr76@gmail.com>
2019-12-03 13:32:34 +0200

    lab2v9.1

--- app.py
+++ app.py
@@ -2,15 +2,17 @@ import base64
 import urllib.request
 
 from flask import Flask, json, request
-from flask_pymongo import PyMongo
 from pymongo import MongoClient
+from firebase_admin import credentials, firestore, initialize_app
 
 app = Flask(__name__)
-# app.config['MONGO_URI'] = "mongodb://oilproductioninfo:GtPvmWddBMjnLCLTvgPETffWGg5ENcDJZg4mSqmmiiiaxyME3DNR3vpwrwaAvqeTV0yTx8XNBTBqtDTWg0PuCw==@oilproductioninfo.documents.azure.com:10255/?ssl=true&replicaSet=globaldb"
-# mongo = PyMongo(app)
-client = MongoClient("mongodb://oilproductioninfo.documents.azure.com:10255/?ssl=true")
-db = client.test
-db.authenticate(name="oilproductioninfo",password="GtPvmWddBMjnLCLTvgPETffWGg5ENcDJZg4mSqmmiiiaxyME3DNR3vpwrwaAvqeTV0yTx8XNBTBqtDTWg0PuCw==")
+# client = MongoClient("mongodb://oilproductioninfo.documents.azure.com:10255/?ssl=true")
+# db = client.test
+# db.authenticate(name="oilproductioninfo",password="GtPvmWddBMjnLCLTvgPETffWGg5ENcDJZg4mSqmmiiiaxyME3DNR3vpwrwaAvqeTV0yTx8XNBTBqtDTWg0PuCw==")
+cred = credentials.Certificate("key.json")
+default_app = initialize_app(cred)
+db = firestore.client()
+db_ref = db.collection('akerbr')
 
 
 @app.route("/")
@@ -20,8 +22,8 @@ def hello():
 
 @app.route("/api/service/get/all", methods=['GET'])
 def get_all_data():
-    test = db.test
-    data = list(test.find({}))
+    data = [doc.to_dict() for doc in db_ref.stream()]
+
     result_data = {}
     for key, value in enumerate(data):
         result_data[key] = dict(list(value.items())[1:])
@@ -33,15 +35,13 @@ def get_all_data():
 
 @app.route("/api/service/post", methods=['GET', 'POST'])
 def post_data():
-    test = db.test
     data = request.form['data']
-    encoded_string = ''
+
     urllib.request.urlretrieve('https://image.freepik.com/free-vector/_1378-197.jpg', 'image.bmp')
     with open("image.bmp", "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read()).decode('ascii')
     data = json.loads(data)
     data['image'] = encoded_string
-    test.insert(data)
-    d = list(test.find({}))
+    db_ref.document().set(data)
     return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
 
